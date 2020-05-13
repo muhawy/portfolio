ec287dd401a5b4c2f083bbab068e306a989ba34e
alwyDev <alwy.pro@gmail.com>
2020-05-13 11:35:15 +0700

    added rubberband function and a bit of css adjustment

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,13 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+// Animate the text in home
+$(document).ready(function () {
+    var letters = $('h1').text();
+    var nHTML = '';
+    for (var letter of letters) {
+        nHTML += "<span class='a'>" + letter + "</span>";
+    }
+    $('h1').html(nHTML);
+})
